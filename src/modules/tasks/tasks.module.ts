ef96fc3c74ced71541b909773239f01b19498859
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 19:00:03 +0330

    fix: fix add Entity to Module Bug

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { Task } from "./enitities/task.entity";
 import { TasksController } from "./tasks.controller";
 import { TasksService } from "./tasks.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [Task],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
   providers: [TasksService],
 })
