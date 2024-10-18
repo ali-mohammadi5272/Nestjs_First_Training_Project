691685375204d73187346b3e008a0d404df395a6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 20:58:12 +0330

    feat: create createOne method to PojectsService

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { ProjectsService } from "./projects.service";
+import { Project } from "./enitities/project.entity";
+import { CreateProjectDTO } from "./dto/createProject.dto";
 
 @Controller("api/projects")
 export class ProjectsController {
   constructor(private readonly projectService: ProjectsService) {}
+
+  @Get()
+  getAll(): Promise<Project[]> {
+    return this.projectService.getAll();
+  }
+
+  @Post()
+  createOneProject(@Body() body: CreateProjectDTO): Promise<Project> {
+    return this.projectService.createOne(body);
+  }
 }
