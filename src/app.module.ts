d0a289c2c111fbcb421531c8d016661c18dadb20
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 19:55:57 +0330

    feat: postgreSQL configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from "@nestjs/common";
 import { ProjectsModule } from "./modules/projects/projects.module";
 import { TasksModule } from "./modules/tasks/tasks.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as path from "path";
 
 @Module({
-  imports: [ProjectsModule, TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      synchronize: true,
+      database: "nestjs_db",
+      type: "postgres",
+      entities: [
+        path.join(__dirname, "modules", "./**/entities/*.entity.{ts,js}"),
+      ],
+    }),
+    ProjectsModule,
+    TasksModule,
+  ],
 })
 export class AppModule {}
