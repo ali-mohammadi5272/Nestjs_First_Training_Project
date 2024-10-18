7b00974684aca30c1a6aec5d50de6664d312f80d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 18:45:11 +0330

    feat: add Entity & Controller & Service to Module

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { Task } from "./enitities/task.entity";
+import { TasksController } from "./tasks.controller";
+import { TasksService } from "./tasks.service";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [Task],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
