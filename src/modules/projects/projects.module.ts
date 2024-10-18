339237c861c7f5560b776b7db9f7d3936f53a9ab
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 18:59:55 +0330

    fix: fix add Entity to Module Bug

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { Project } from "./enitities/project.entity";
 import { ProjectsService } from "./projects.service";
 import { ProjectsController } from "./projects.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [Project],
+  imports: [TypeOrmModule.forFeature([Project])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
