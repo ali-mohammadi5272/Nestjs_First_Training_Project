ca67895b30edad9773bfccccf5eb943271a0f14b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-18 20:59:14 +0330

    feat: create createOne method to PojectsService

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Project } from "./entities/project.entity";
 import { Repository } from "typeorm";
+import { CreateProjectDTO } from "./dto/createProject.dto";
 
 @Injectable()
 export class ProjectsService {
@@ -9,4 +10,9 @@ export class ProjectsService {
     @InjectRepository(Project)
     private _repository: Repository<Project>,
   ) {}
+
+  createOne(createProjectDTO: CreateProjectDTO): Promise<Project> {
+    const newProject = this._repository.create(createProjectDTO);
+    return this._repository.save(newProject);
+  }
 }
