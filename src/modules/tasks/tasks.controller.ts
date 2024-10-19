91b7d4f5009679d8760e18f6d6f78c217057acf7
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-19 16:37:28 +0330

    feat: add createTask to taskController

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { Task } from "./entities/task.entity";
+import { TasksService } from "./tasks.service";
+import { CreateTaskDto } from "./dto/createTask.dto";
 
 @Controller("api/tasks")
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly _taskService: TasksService) {}
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this._taskService.createOne(createTaskDto);
+  }
+}
