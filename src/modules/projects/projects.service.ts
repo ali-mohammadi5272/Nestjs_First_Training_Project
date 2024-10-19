b183d172d947387944b6132de0a692e99dfeb624
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-19 22:10:46 +0330

    feat: add updateOne to projectService

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Project } from "./entities/project.entity";
 import { Repository } from "typeorm";
 import { CreateProjectDTO } from "./dto/createProject.dto";
+import { UpdateProjectDTO } from "./dto/updateProject.dto";
 
 @Injectable()
 export class ProjectsService {
@@ -26,5 +27,15 @@ export class ProjectsService {
 
   deleteOne(id: number): void {
     this._repository.delete({ id });
+  }
+
+  async updateOne(
+    id: number,
+    updateProjectDTO: UpdateProjectDTO,
+  ): Promise<Project> {
+    const test = await this._repository.update({ id }, updateProjectDTO);
+    console.log(test);
+    const updatedUser = this._repository.findOneBy({ id });
+    return updatedUser;
   }
 }
