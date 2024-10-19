d8aafc3adb77ff7a8ca603a808968d6e6b01c93c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-19 14:27:23 +0330

    feat: create Tasks Service

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Task } from "./entities/task.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private _repository: Repository<Task>,
+  ) {}
+}
